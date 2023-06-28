9664c4c2eaf427eec98dfc1949377d95a1333816
Kevin Ziegler <kziegler@ojolabs.com>
2023-06-28 12:07:39 -0500

    Update styles for RecursiveBinder spoon

--- styles.lua
+++ styles.lua
@@ -1,16 +1,16 @@
 -- hs.styledtext.setStyle({ font  = { name = "Overpass Mono", size = 18 } })
-hs.styledtext.font = { name = "Overpass Mono", size = 18 }
+hs.styledtext.font = { name = "Berkeley Mono", size = 18 }
 hs.alert.defaultStyle.radius = 5
 
 recursiveBinderFormat = {
     atScreenEdge = 0,
     padding = 20,
-    radius = 5,
-    textStyle = {
-        font = {
-            name = "Overpass Mono",
-            size = 18
-        }
-    }
+    textColor = { hex = "CBCCC6" },
+    strokeWidth = 5,
+    -- strokeColor = { hex = "#171B24", alpha = 1.0 },
+    strokeColor = { hex = "#CBCCC6", alpha = 1.0 },
+    fillColor = { hex = "#1F2430", alpha = 0.95 },
+    radius = 7,
+    textStyle = { font = { name = "Berkeley Mono", size = 18 } }
 }
 
