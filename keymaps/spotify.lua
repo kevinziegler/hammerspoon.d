03ace7a440d5d4fa729edce58de2a3b69d65cce7
Kevin Ziegler <kevinziegler@Balder.asgard>
2023-09-15 12:53:09 -0500

    Clean up unused config code

--- keymaps/spotify.lua
+++
@@ -1,7 +0,0 @@
-return function(binder)
-  return {
-    [binder.singleKey("space", "Play/Pause")] = hs.spotify.playpause,
-    [binder.singleKey("n", "Play Next")] = hs.spotify.next,
-    [binder.singleKey("p", "Play Previous")] = hs.spotify.previous,
-  }
-end
--- window-management/init.lua
+++
@@ -1,6 +0,0 @@
-local windowManagement = {}
-
-windowManagement.Partition = require("window-management.partition")
-windowManagement.LayoutUltrawide = require("window-management.layout-ultrawide")
-
-return windowManagement
